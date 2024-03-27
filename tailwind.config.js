b879e4ed45c3a94da89b78ce0ab8f150c994b21f
A3thyr <a3therys@gmail.com>
2024-03-27 11:44:43 +0300

    init: initial commmit(with few features)

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontSize: {
+      "2xs": "10px",
+      xs: "12px",
+      s: "13px",
+      reg: "15px",
+      lg: "18px",
+      "2xl": "22px",
+      "3xl": "25px",
+      "4xl": "32px",
+      "5xl": "45px",
+      "6xl": "60px",
+      "7xl": "75px",
+      "8xl": "90px",
+    },
+  },
+  plugins: [],
+};
