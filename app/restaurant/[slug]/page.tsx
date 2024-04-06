59aedc432854a7c2d193130c983f9f4c957d63c5
A3thyr <a3therys@gmail.com>
2024-04-06 13:22:45 +0300

    feat: added availability logic within reseravtion card, fixed models for Table, Booking, Restaurant via prisma

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -61,6 +61,7 @@ export default async function RestaurantDetails({
       </div>
       <div className="w-[27%] relative text-reg">
         <ReservationCard
+          slug={restaurant.slug}
           openTime={restaurant.open_time}
           closeTime={restaurant.close_time}
         />
