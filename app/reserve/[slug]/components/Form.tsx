1761fea8de06414c4f3bbef622b5d0fdbede7398
A3thyr <a3therys@gmail.com>
2024-04-08 14:00:20 +0300

    feat: implemented reservation and booking logic. main production is done, some features MAY BE in the future

--- app/reserve/[slug]/components/Form.tsx
+++ app/reserve/[slug]/components/Form.tsx
@@ -1,46 +1,144 @@
-import { FC } from "react";
+"use client";
 
-const Form: FC = () => {
+import { ChangeEvent, useEffect, useState } from "react";
+import useReservation from "../../../../hooks/useReservation";
+import { CircularProgress } from "@mui/material";
+
+const Form = ({
+  slug,
+  partySize,
+  date,
+}: {
+  slug: string;
+  partySize: string;
+  date: string;
+}) => {
+  const [inputs, setInputs] = useState({
+    bookerFirstName: "",
+    bookerLastName: "",
+    bookerPhone: "",
+    bookerEmail: "",
+    bookerOccasion: "",
+    bookerRequest: "",
+  });
+
+  const [day, time] = date.split("T");
+
+  const [disabled, setDisabled] = useState(true);
+  const [didBook, setDidBook] = useState(false);
+  const { loading, error, createReservation } = useReservation();
+
+  useEffect(() => {
+    if (
+      inputs.bookerFirstName &&
+      inputs.bookerLastName &&
+      inputs.bookerPhone &&
+      inputs.bookerEmail
+    ) {
+      return setDisabled(false);
+    } else {
+      return setDisabled(true);
+    }
+  }, [inputs]);
+
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleClick = async () => {
+    const booking = await createReservation({
+      slug,
+      partySize,
+      time,
+      day,
+      bookerFirstName: inputs.bookerFirstName,
+      bookerLastName: inputs.bookerLastName,
+      bookerPhone: inputs.bookerPhone,
+      bookerEmail: inputs.bookerEmail,
+      bookerOccasion: inputs.bookerOccasion,
+      bookerRequest: inputs.bookerRequest,
+      setDidBook,
+    });
+  };
+
   return (
     <div className="mt-10 flex flex-wrap justify-between w-[660px]">
-      <input
-        type="text"
-        className="border rounded p-3 w-80 mb-4"
-        placeholder="First name"
-      />
-      <input
-        type="text"
-        className="border rounded p-3 w-80 mb-4"
-        placeholder="Last name"
-      />
-      <input
-        type="text"
-        className="border rounded p-3 w-80 mb-4"
-        placeholder="Phone number"
-      />
-      <input
-        type="text"
-        className="border rounded p-3 w-80 mb-4"
-        placeholder="Email"
-      />
-      <input
-        type="text"
-        className="border rounded p-3 w-80 mb-4"
-        placeholder="Occasion (optional)"
-      />
-      <input
-        type="text"
-        className="border rounded p-3 w-80 mb-4"
-        placeholder="Requests (optional)"
-      />
-      <button className="bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300">
-        Complete reservation
-      </button>
-      <p className="mt-4 text-sm">
-        By clicking “Complete reservation” you agree to the YourForks Terms of
-        Use and Privacy Policy. Standard text message rates may apply. You may
-        opt out of receiving text messages at any time.
-      </p>
+      {didBook ? (
+        <div>
+          <h1>You are all booked up</h1>
+          <p>Enjoy your meal!</p>
+        </div>
+      ) : (
+        <>
+          <input
+            type="text"
+            className="border rounded p-3 w-80 mb-4"
+            placeholder="First name"
+            name="bookerFirstName"
+            value={inputs.bookerFirstName}
+            onChange={handleChangeInput}
+          />
+          <input
+            type="text"
+            className="border rounded p-3 w-80 mb-4"
+            placeholder="Last name"
+            name="bookerLastName"
+            value={inputs.bookerLastName}
+            onChange={handleChangeInput}
+          />
+          <input
+            type="text"
+            className="border rounded p-3 w-80 mb-4"
+            placeholder="Phone number"
+            name="bookerPhone"
+            value={inputs.bookerPhone}
+            onChange={handleChangeInput}
+          />
+          <input
+            type="text"
+            className="border rounded p-3 w-80 mb-4"
+            placeholder="Email"
+            name="bookerEmail"
+            value={inputs.bookerEmail}
+            onChange={handleChangeInput}
+          />
+          <input
+            type="text"
+            className="border rounded p-3 w-80 mb-4"
+            placeholder="Occasion (optional)"
+            name="bookerOccasion"
+            value={inputs.bookerOccasion}
+            onChange={handleChangeInput}
+          />
+          <input
+            type="text"
+            className="border rounded p-3 w-80 mb-4"
+            placeholder="Requests (optional)"
+            name="bookerRequest"
+            value={inputs.bookerRequest}
+            onChange={handleChangeInput}
+          />
+          <button
+            disabled={disabled || loading}
+            onClick={handleClick}
+            className="bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300"
+          >
+            {loading ? (
+              <CircularProgress color="inherit" />
+            ) : (
+              "Complete reservation"
+            )}
+          </button>
+          <p className="mt-4 text-sm">
+            By clicking “Complete reservation” you agree to the YourForks Terms
+            of Use and Privacy Policy. Standard text message rates may apply.
+            You may opt out of receiving text messages at any time.
+          </p>
+        </>
+      )}
     </div>
   );
 };
--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,12 +1,46 @@
 import ReserveHeader from "./components/ReserveHeader";
 import Form from "./components/Form";
+import { PrismaClient } from "@prisma/client";
+import { notFound } from "next/navigation";
 
-export default function ReservationPage() {
+const prisma = new PrismaClient();
+
+const fetchRestaurantsBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+  });
+
+  if (!restaurant) {
+    notFound();
+  }
+
+  return restaurant;
+};
+
+export default async function ReservationPage({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { date: string; partySize: string };
+}) {
+  const restaurant = await fetchRestaurantsBySlug(params.slug);
   return (
     <div className="border-t h-screen">
       <div className="py-9 w-3/5 m-auto">
-        <ReserveHeader />
-        <Form />
+        <ReserveHeader
+          image={restaurant.main_image}
+          name={restaurant.name}
+          date={searchParams.date}
+          partySize={searchParams.partySize}
+        />
+        <Form
+          date={searchParams.date}
+          partySize={searchParams.partySize}
+          slug={params.slug}
+        />
       </div>
     </div>
   );
